0c7030fb5424db16eee49834e34d0df5fc9b262a
Henrique Barbosa <henrique.santos@protonsistemas.com.br>
2021-12-26 12:13:48 -0300

    Alter

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import { Home } from './src/screens/Home';
 import { CarDetails } from './src/screens/CarDetails';
 import { Scheduling } from './src/screens/Scheduling';
 import { SchedulingDetails } from './src/screens/SchedulingDetails';
+import { SchedulingComplete } from './src/screens/SchedulingComplete'
 
 import theme from './src/styles/theme';
 
@@ -34,7 +35,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }
