d7f334659769b879e7a4cabfdbd9cd252d242e69
phprograming <paulo_hbs17@hotmail.com>
2022-01-26 23:06:34 -0300

    Alter aulas

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import { createStackNavigator  } from '@react-navigation/stack';
 
-import { Home } from '../screens/Home';
-import { CarDetails } from '../screens/CarDetails';
-import { Scheduling } from '../screens/Scheduling';
-import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { Confirmation } from '../screens/Confirmation';
-import { MyCars } from '../screens/MyCars';
 import { Splash } from '../screens/Splash';
 import { SignIn } from '../screens/SignIn';
 import { SignUpFirstStep } from '../screens/SignUp/SignUpFirstStep';
 import { SignUpSecondStep } from '../screens/SignUp/SignUpSecondStep';
 
-export function StackRoutes(){
+export function AuthRoutes(){
     const { Navigator, Screen } = createStackNavigator();
 
     return(
         <Navigator 
             screenOptions={{ headerShown: false }}
-            initialRouteName="SignIn"
+            initialRouteName="Splash"
         >
+            <Screen 
+                name="Splash"
+                component={Splash}
+            />
             <Screen 
                 name="SignIn"
                 component={SignIn}
@@ -31,33 +30,10 @@ export function StackRoutes(){
             <Screen 
                 name="SignUpSecondStep"
                 component={SignUpSecondStep}
-            />            
-            <Screen 
-                name="Home"
-                component={Home}
-                options={{
-                    gestureEnabled: false,
-                }}
-            />
-            <Screen 
-                name="CarDetails"
-                component={CarDetails}
-            />
-            <Screen 
-                name="Scheduling"
-                component={Scheduling}
-            />
-            <Screen 
-                name="SchedulingDetails"
-                component={SchedulingDetails}
-            />
+            />   
             <Screen 
                 name="Confirmation"
                 component={Confirmation}
-            />
-            <Screen 
-                name="MyCars"
-                component={MyCars}
             />
         </Navigator>
     )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { useAuth } from '../hooks/auth';
 
-import { StackRoutes } from './stack.routes';
+import { AppTabRoutes } from './app.tab.routes';
+import { AuthRoutes } from './auth.routes';
 
 export function Routes(){
+    const { user } = useAuth();
+
     return(
         <GestureHandlerRootView style={{ flex: 1 }}>
             <NavigationContainer>
-                <StackRoutes />
+                { user ?  <AppTabRoutes /> : <AuthRoutes /> }
             </NavigationContainer>
         </GestureHandlerRootView>
     );
