97465b7caabc0cd59efd8a5b867deedd3211c4f0
phprograming <paulo_hbs17@hotmail.com>
2022-02-09 21:31:36 -0300

    Alter

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,22 +2,22 @@ import React, {
     createContext,
     useState,
     useContext,
-    ReactNode
+    ReactNode,
+    useEffect
 } from 'react';
 
 import { api } from '../services/api';
+import { database } from '../database';
+import { User as ModelUser } from '../database/model/User';
 
 interface User {
     id: string;
+    user_id: String;
     email: string;
     name: string;
     driver_license: string;
     avatar: string;
-}
-
-interface AuthState {
     token: string;
-    user: User;
 }
 
 interface SighInCredentials {
@@ -37,25 +37,58 @@ interface AuthProviderProps {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children } : AuthProviderProps){
-    const [data, setData] = useState<AuthState>({} as AuthState);
+    const [data, setData] = useState<User>({} as User);
 
     async function sighIn({ email, password } : SighInCredentials) {
-        const response = await api.post('/sessions', {
-            email,
-            password
-        });
+        try {
+            const response = await api.post('/sessions', {
+                email,
+                password
+            });
+    
+            const { token, user } = response.data;
+    
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
-        const { token, user } = response.data;
-
-        api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
-        setData({ token, user });        
+            const userCollection = database.get<ModelUser>('users');
+            await database.write(async () => {
+                await userCollection.create(( newUser ) => {
+                    newUser.user_id = user.id,
+                    newUser.name = user.name,
+                    newUser.email = user.email,
+                    newUser.driver_license = user.driver_license,
+                    newUser.avatar = user.avatar,
+                    newUser.token = user.token
+                });
+            });
+    
+            setData({ ...user, token }); 
+        } catch(error) {
+            throw new Error(error);
+        } 
     }
+
+    useEffect(() => {
+        async function loadUserData() {
+            const userCollection = database.get<ModelUser>('users');
+            const response = await userCollection.query().fetch();
+
+            console.log(response);
+
+            if(response.length > 0){
+                const userData = response[0]._raw as unknown as User;
+                api.defaults.headers.common['Authorization'] = `Bearer ${userData.token}`;
+                setData(userData);
+            }
+        }
+
+        loadUserData();
+    });
 
     return (
         <AuthContext.Provider 
             value={{
-                user: data.user,
+                user: data,
                 sighIn
             }}
         >
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,7 +12,7 @@ export function Routes(){
     return(
         <GestureHandlerRootView style={{ flex: 1 }}>
             <NavigationContainer>
-                { user ?  <AppTabRoutes /> : <AuthRoutes /> }
+                { user.id ?  <AppTabRoutes /> : <AuthRoutes /> }
             </NavigationContainer>
         </GestureHandlerRootView>
     );
